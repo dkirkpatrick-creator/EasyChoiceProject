fefdc3bce63ac3a4d0ea2b57022f3ca299103a82
Kyle Daniel Kirkpatrick <59846329+dkirkpatrick-creator@users.noreply.github.com>
2020-11-24 19:11:30 -0600

    Made the first page, still need to fix up some naming conventions and make it so that the buttons actually do stuff

--- EasyChoice/EasyChoice/MainPage.xaml.cs
+++ EasyChoice/EasyChoice/MainPage.xaml.cs
@@ -1,18 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using Xamarin.Forms;
 
 namespace EasyChoice
 {
     public partial class MainPage : ContentPage
     {
+        string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "easychoice.txt");
+
         public MainPage()
         {
             InitializeComponent();
+
+            if (File.Exists(_fileName))
+            {
+                // implement
+            }
+        }
+
+        void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            // implement
+        }
+
+        void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            if (File.Exists(_fileName))
+            {
+                File.Delete(_fileName);
+            }
+            // implement
         }
     }
 }
